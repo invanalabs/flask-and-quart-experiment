de1bab38e8fd3f1a0309e2f222df24a7967c9354
Ravi Raja Merugu <rrmerugu@gmail.com>
2020-04-15 10:57:46 +0530

    quart uvloop attempts commented

--- quart-hello-world/quart_app.py
+++ quart-hello-world/quart_app.py
@@ -4,13 +4,16 @@
 pip install quart
 
 # command to start the production like hypercorn server
-hypercorn quart_app:app
+ hypercorn quart_app:app --workers 12 --debug
 """
 
 from quart import Quart, websocket
+# import uvloop
+# import asyncio
 
 app = Quart(__name__)
 
+# asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 @app.route('/')
 async def hello_world():
