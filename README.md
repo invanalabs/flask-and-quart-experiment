4bf3b1627bddc0670c8343e70ab8827a01380fa1
Ravi Raja Merugu <rrmerugu@gmail.com>
2020-04-15 09:13:42 +0530

    requirements and readme added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Flask & Quart Experiment 
+# Flask & Quart Performance Experiment 
 
-
+This is an attempt to understand the performance of Micro Web frameworks 
+in python - [Flask](https://github.com/pallets/flask) and [Quart](https://gitlab.com/pgjones/quart). 
+Quart is a Python ASGI web microframework with the same API as Flask.
--- quart-hello-world/quart_app.py
+++ quart-hello-world/quart_app.py
@@ -3,9 +3,8 @@
 # prerequisites
 pip install quart
 
-# command to start the production like flask server
-uwsgi --socket 0.0.0.0:5000 --protocol=http -w flask_app:app
-
+# command to start the production like hypercorn server
+hypercorn quart_app:app
 """
 
 from quart import Quart, websocket
@@ -16,12 +15,6 @@ app = Quart(__name__)
 @app.route('/')
 async def hello_world():
     return {"message": "hello"}
-
-
-@app.websocket('/ws')
-async def ws():
-    while True:
-        await websocket.send('hello')
 
 
 if __name__ == "__main__":
